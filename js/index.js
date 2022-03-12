291b701cb35e2cc09a7caa888e454747ab06d40f
ZIHAD22 <mdzihad2244@gmail.com>
2022-03-13 01:11:05 +0600

    console log select element

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const convertFrom = document.getElementById('convertFrom')
+var e = document.getElementById('elementId')
+var value = convertFrom.options[convertFrom.selectedIndex].value
+console.log(value)
